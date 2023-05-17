3ceca4cc5e2535918d8205be82abaf25cb52bf6a
webcrunch <jarllinduist@gmail.com>
2023-05-17 16:00:34 +0200

    WIP: handling course info like chapters and lessions

--- source/js/populate_data.js
+++ source/js/populate_data.js
@@ -1,10 +1,20 @@
 
-
+const courses = async () => !localStorage.getItem("courses") ? localStorage.setItem("courses", [{ name: "Introduction to DevOps Practices:", chapters: 4 }, { name: "Introduction to Agile Development:", chapters: 5 }]) : null;
+const chapters = async () => !localStorage.getItem("chapters") ? localStorage.setItem("chapters", JSON.stringify(await fetchChaps())): null;
 const checkIfusers = async () => !localStorage.getItem("users") ? localStorage.setItem("users", JSON.stringify(await fetchUsers())) : null;
 
 const fetchUsers = async () => {
     return await (fetch('/assets/people.json')
         .then((response) => response.json()));
 }
+
+
+const fetchChaps = async () => {
+    return await (fetch('/assets/chapters.json')
+        .then((response) => response.json()));
+}
+
+
+
 
 document.addEventListener('DOMContentLoaded', checkIfusers, false);
