96b8926e331b5f07c8e0fadacecc4e88d36c4440
webcrunch <jarllinduist@gmail.com>
2023-05-25 14:37:45 +0200

    course and lesson progress bar

--- source/js/courses-fininshed.js
+++ source/js/courses-fininshed.js
@@ -1,7 +1,7 @@
 const user = JSON.parse(localStorage.getItem("session"));
 const chapters = JSON.parse(localStorage.getItem("chapters"))
 const chapterFocused = localStorage.getItem("courseClicked");
-
+const progress = JSON.parse(localStorage.getItem("progress"));
 const populateData = () => {
     document.querySelector('.titleCourse').innerHTML = `Courses for ${chapterFocused}`
     document.querySelector('h2').innerHTML = `User: ${user.first_name}  ${user.last_name}`;
@@ -9,23 +9,30 @@ const populateData = () => {
     document.querySelector('.user_name').innerHTML = ` Log out: ${user.first_name} `;
     // document.querySelector('.user_name').addEventListener("click", logout())
     let ul = document.querySelector('.chapters');
-    
-    chapters.filter(chapt => chapt.course ===  chapterFocused).forEach(chapt => {
+
+    let userProgress = progress.filter(prog => user.id === prog.id)
+
+    userProgress = userProgress[0];
+
+    chapters.filter(chapt => chapt.course === chapterFocused).forEach(chapt => {
         let li = document.createElement("li");
-            
+        let progressEach = userProgress.progress.find(prog => prog["chapter-name"] === chapt.name);
         li.appendChild(document.createTextNode(`${chapt.name} `));
+        let lengthOfLessions = chapt.lession.length;
+        let calculatingTotalLessions = progressEach.lessions.map(a => a.progress).reduce((a, b) => a + b, 0);
         let pros = document.createElement("PROGRESS");
-            pros.setAttribute("value", "42");
+        let calculatingProcent = calculatingTotalLessions / lengthOfLessions;
+        pros.setAttribute("value", Number.isInteger(calculatingProcent) ? calculatingProcent : calculatingProcent.toFixed(1));
         pros.setAttribute("max", "100");
-        
-        ul.appendChild(li); 
+        ul.appendChild(li);
         ul.appendChild(pros);
         chapt.lession.forEach(less => {
+            let lessEach = progressEach.lessions.find(prog => prog.name === less.name);
             let ulInside = document.createElement("ul");
             ul.appendChild(ulInside);
             let li = document.createElement("li");
-            let pros = document.createElement("PROGRESS");
-            pros.setAttribute("value", "42");
+            let pros = document.createElement("progress");
+            pros.setAttribute("value", lessEach.progress);
             pros.setAttribute("max", "100");
             li.appendChild(document.createTextNode(`${less.name}`));
             ulInside.appendChild(li);
