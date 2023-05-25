7a183f58e797f96bc0490785c1d04afbcd50a08e
webcrunch <jarllinduist@gmail.com>
2023-05-25 10:18:57 +0200

    all chapters/lessions for the current course and how much that is done

--- source/js/courses-fininshed.js
+++ source/js/courses-fininshed.js
@@ -1,32 +1,44 @@
 const user = JSON.parse(localStorage.getItem("session"));
 const chapters = JSON.parse(localStorage.getItem("chapters"))
-
+const chapterFocused = localStorage.getItem("courseClicked");
 
 const populateData = () => {
-    document.querySelector('h2').innerHTML = `${user.first_name}  ${user.last_name}`;
-    document.querySelector('h3').innerHTML = `${user.email}`;
-
+    document.querySelector('.titleCourse').innerHTML = `Courses for ${chapterFocused}`
+    document.querySelector('h2').innerHTML = `User: ${user.first_name}  ${user.last_name}`;
+    document.querySelector('h3').innerHTML = `Email: ${user.email}`;
+    document.querySelector('.user_name').innerHTML = ` Log out: ${user.first_name} `;
+    // document.querySelector('.user_name').addEventListener("click", logout())
     let ul = document.querySelector('.chapters');
     
-    
-    
-    chapters.forEach(chapt => {
+    chapters.filter(chapt => chapt.course ===  chapterFocused).forEach(chapt => {
         let li = document.createElement("li");
+            
+        li.appendChild(document.createTextNode(`${chapt.name} `));
+        let pros = document.createElement("PROGRESS");
+            pros.setAttribute("value", "42");
+        pros.setAttribute("max", "100");
         
-    li.appendChild(document.createTextNode(`${chapt.name} - 0%`));
         ul.appendChild(li); 
+        ul.appendChild(pros);
         chapt.lession.forEach(less => {
             let ulInside = document.createElement("ul");
             ul.appendChild(ulInside);
-                let li = document.createElement("li");
-            li.appendChild(document.createTextNode(`${less.name} - 0%`));
+            let li = document.createElement("li");
+            let pros = document.createElement("PROGRESS");
+            pros.setAttribute("value", "42");
+            pros.setAttribute("max", "100");
+            li.appendChild(document.createTextNode(`${less.name}`));
             ulInside.appendChild(li);
+            ulInside.appendChild(pros);
         })
     })
-    
-    // ul.innerHTML += `<li> four </li>`;
-    
 }
+
+const logout = () => {
+    localStorage.setItem("session", null);
+    window.location.href = "/";
+}
+
 
 
 
