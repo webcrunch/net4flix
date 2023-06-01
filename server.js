d40183299221bfbdfd91d44f7e1de23c24c06000
draginojd <arminfazlikhan@gmail.com>
2023-06-01 14:57:40 +0200

    installed mysql2 & connected server to database

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const path = require("path");
+const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
 const port = 5000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/source/html/schedule/schedule.html'));
+const sequelize = new Sequelize('net4flix', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const User = sequelize.define('users', {
+  // assuming your users table has these fields
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/schedule", (req,res) => {
+  res.sendFile(path.join(__dirname, "public/source/html/pages/schedule/schedule.html"));
+});
+
+sequelize.sync()
+.then(() =>{
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+})
+.catch((err) => {
+  console.error('Unable to connect to the database:', err);
 });
